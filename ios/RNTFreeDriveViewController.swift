ee1185c69b0742765cf884802aa46752f85607f9
Micah Effiong <micaiah.effiong@gmail.com>
2023-08-08 15:03:43 +0100

    show user location during free driver. IT WORKS

--- ios/RNTFreeDriveViewController.swift
+++ ios/RNTFreeDriveViewController.swift
@@ -16,29 +16,18 @@ import MapboxMaps
 @objc(RNTFreeDriveViewController)
 class RNTFreeDriveViewController: UIViewController {
     
-    private lazy var navigationMapView = NavigationMapView(frame: view.bounds)
-    private let toggleButton = UIButton()
+    private lazy var navigationMapView = NavigationMapView()
     private let passiveLocationManager = PassiveLocationManager()
     private lazy var passiveLocationProvider = PassiveLocationProvider(locationManager: passiveLocationManager)
-    
-    // Create the view
-    let speedLimitView = SpeedLimitView()
-    
-    private var isSnappingEnabled: Bool = false {
-        didSet {
-            toggleButton.backgroundColor = isSnappingEnabled ? .blue : .darkGray
-            let locationProvider: LocationProvider = isSnappingEnabled ? passiveLocationProvider : AppleLocationProvider()
-            navigationMapView.mapView.location.overrideLocationProvider(with: locationProvider)
-            passiveLocationProvider.startUpdatingLocation()
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationMapView()
-        view.addSubview(speedLimitView)
-        setupSnappingToggle()
+        
+        let locationProvider: LocationProvider = passiveLocationProvider
+        navigationMapView.mapView.location.overrideLocationProvider(with: locationProvider)
+        passiveLocationProvider.startUpdatingLocation()
     }
     
     private func setupNavigationMapView() {
@@ -49,25 +38,6 @@ class RNTFreeDriveViewController: UIViewController {
         navigationViewportDataSource.options.followingCameraOptions.zoomUpdatesAllowed = false
         navigationViewportDataSource.followingMobileCamera.zoom = 17.0
         navigationMapView.navigationCamera.viewportDataSource = navigationViewportDataSource
-        
         view.addSubview(navigationMapView)
-    }
-    
-    private func setupSnappingToggle() {
-        toggleButton.setTitle("Snap to Roads", for: .normal)
-        toggleButton.layer.cornerRadius = 5
-        toggleButton.translatesAutoresizingMaskIntoConstraints = false
-        isSnappingEnabled = false
-        toggleButton.addTarget(self, action: #selector(toggleSnapping), for: .touchUpInside)
-        view.addSubview(toggleButton)
-        toggleButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
-        toggleButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        toggleButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
-        toggleButton.sizeToFit()
-        toggleButton.titleLabel?.font = UIFont.systemFont(ofSize: 25)
-    }
-    
-    @objc private func toggleSnapping() {
-        isSnappingEnabled.toggle()
     }
 }
